9a1704db9a480585f5945f76e9d9ac1096025ab1
Kévin Mazé <kekeproject@hotmail.fr>
2026-05-12 19:52:26 +0200

    init project

---
+++ app/components/Nav.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Link from "next/link";
+import Logo from "@/public/logo.svg";
+import Image from "next/image";
+import { ModeToggle } from "./ThemeToggle";
+
+export default function Nav() {
+    return (
+        <nav className="max-w-[1200px] w-full mx-auto h-[80px] flex items-center justify-between p-5 border-b border-gray-300">
+            <div>
+                <Link href="/">
+                    <Image
+                        src={Logo}
+                        alt="Logo du site MDC SAAS"
+                        width={30}
+                        height={30}
+                        className="w-12 h-12"
+                    />
+                </Link>
+            </div>
+
+            <div className="flex items-center gap-5">
+                {" "}
+                <ModeToggle />{" "}
+            </div>
+        </nav>
+    );
+}
+
+// 27:15
