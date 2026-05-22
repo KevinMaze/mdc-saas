9749a93cebce8578884cf13996d4a91dab2042ab
MAZE Kevin <kekeproject@hotmail.fr>
2026-05-22 13:29:10 +0200

    add typewriter and mode dark

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -19,7 +19,7 @@ export default function Nav() {
                 </Link>
             </div>
 
-            <div className="flex items-center gap-5">
+            <div className="flex items-center gap-4">
                 {" "}
                 <ModeToggle />{" "}
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,30 @@
+"use client";
+import Link from "next/link";
+import Logo from "@/public/logo.svg";
 import Image from "next/image";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function Home() {
-    return <>hello</>;
+    return (
+        <>
+            <section className="w-full h-screen flex items-center justify-center flex-col gap-2">
+                <Image
+                    width={100}
+                    height={100}
+                    alt="Logo"
+                    src={Logo}
+                    className="mb-4 object-contain"
+                />
+                <h1 className="text-4xl md:text-6xl font-black mb-2 text-center uppercase flex items-center">
+                    <Typewriter
+                        typeSpeed={50}
+                        words={["Bienvenue", "Welcome"]}
+                        loop={true}
+                        cursor
+                        cursorStyle="|"
+                    />
+                </h1>
+            </section>
+        </>
+    );
 }
